33038656abc96e9a632128ad3e905ed6aa8f2ef4
Denis Davydov <dvd9@yandex.ru>
2022-07-12 11:54:02 +0300

    4. Form с композиционным объектом [#305522]

--- src/main/java/ru/job4j/accident/store/AccidentMem.java
+++ src/main/java/ru/job4j/accident/store/AccidentMem.java
@@ -2,31 +2,30 @@ package ru.job4j.accident.store;
 
 import org.springframework.stereotype.Repository;
 import ru.job4j.accident.model.Accident;
+import ru.job4j.accident.model.Rule;
 
 import java.util.Collection;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @Repository
 public class AccidentMem {
-    AtomicInteger id = new AtomicInteger(0);
+    private final AtomicInteger id = new AtomicInteger(0);
 
-    private final AccidentTypeMem accidentTypeMem;
-
-    Map<Integer, Accident> accidents = new ConcurrentHashMap<>();
+    private final Map<Integer, Accident> accidents = new ConcurrentHashMap<>();
 
     public AccidentMem(AccidentTypeMem accidentTypeMem) {
-        this.accidentTypeMem = accidentTypeMem;
         accidents.put(id.incrementAndGet(),
                 new Accident(id.get(), "Accident1", "Description", "Moscow",
-                        accidentTypeMem.findAccidentTypeById(1)));
+                        accidentTypeMem.findAccidentTypeById(1), Set.of(Rule.of(1, "Статья 1"))));
         accidents.put(id.incrementAndGet(),
                 new Accident(id.get(), "Accident2", "Description", "Spb",
-                        accidentTypeMem.findAccidentTypeById(2)));
+                        accidentTypeMem.findAccidentTypeById(2), Set.of(Rule.of(2, "Статья 2"))));
         accidents.put(id.incrementAndGet(),
                 new Accident(id.get(), "Accident3", "Description", "Ekb",
-                        accidentTypeMem.findAccidentTypeById(3)));
+                        accidentTypeMem.findAccidentTypeById(3), Set.of(Rule.of(3, "Статья 3"))));
     }
 
     public Collection<Accident> findAll() {
