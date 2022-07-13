a3eb776734882768ce15f815e5c52fdc2198a000
Denis Davydov <dvd9@yandex.ru>
2022-07-13 21:22:29 +0300

    0. Spring DataSource [#6878]

---
+++ db/schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE accident (
+    id serial primary key,
+    name varchar(2000)
+);
+
+INSERT INTO accident (name) VALUES('accident 1');
+INSERT INTO accident (name) VALUES('accident 2');
+INSERT INTO accident (name) VALUES('accident 3');
--- src/main/java/ru/job4j/accident/controller/IndexControl.java
+++ src/main/java/ru/job4j/accident/controller/IndexControl.java
@@ -3,23 +3,33 @@ package ru.job4j.accident.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import ru.job4j.accident.model.Accident;
 import ru.job4j.accident.service.AccidentService;
+import ru.job4j.accident.store.AccidentJdbcTemplate;
 
 
 @Controller
 public class IndexControl {
+   /**
     private final AccidentService service;
-
     public IndexControl(AccidentService service) {
         this.service = service;
+    }
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("accidents", service.findAll());
+        return "index";
+    }
+    */
+
+    private final AccidentJdbcTemplate accidents;
+
+    public IndexControl(AccidentJdbcTemplate accidents) {
+        this.accidents = accidents;
     }
 
     @GetMapping("/")
     public String index(Model model) {
-        model.addAttribute("accidents", service.findAll());
+        model.addAttribute("accidents", accidents.getAll());
         return "index";
     }
 }
